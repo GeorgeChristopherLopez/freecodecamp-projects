958b28033d8688052bfd1fc3456eb1cd6d7035ed
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-08-08 21:40:30 -0400

    completed build-a-markdown-previewer

--- Front-End-Libraries-Projects/build-a-markdown-previewer/dist/script.js
+++ Front-End-Libraries-Projects/build-a-markdown-previewer/dist/script.js
@@ -1,17 +1,18 @@
 function Editor(props) {
 
   return React.createElement("section", { className: "editor-section" },
-  React.createElement("div", { className: "editor-container" }, React.createElement("div", { className: "editor-header" }, React.createElement("span", null, React.createElement("button", null, "!"), "Editor"), React.createElement("button", { className: "resizeBtn" }, "X")), React.createElement("textarea", { onChange: props.onChange, id: "editor" }, "this is a test of the emergency broadcast center")));
-
+  React.createElement("div", { className: "editor-container" }, React.createElement("div", { className: "editor-header" }, React.createElement("span", null, React.createElement("button", null, "!"), "Editor"), React.createElement("button", { className: "resizeBtn" }, "X")), React.createElement("textarea", { onChange: props.onChange, id: "editor" },
+  placeholder)));
 
 
 
 }
 
 function Preview(props) {
+
   const text = props.display;
   function getMarkdownText() {
-    var rawMarkup = marked(text, { sanitize: true });
+    var rawMarkup = marked(text);
     return { __html: rawMarkup };
   }
 
@@ -26,27 +27,24 @@ function Preview(props) {
 
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      text: "this is a test of the emergency state and props system" };
+      text: placeholder };
 
 
     this.handleChange = this.handleChange.bind(this);
-    this.getMarkdownText = this.getMarkdownText.bind(this);
+
   }
 
 
   handleChange() {
-    let mark = this.getMarkdownText();
+
     this.setState(state => ({
       text: event.target.value }));
 
 
-  }
-  getMarkdownText() {
-    var rawMarkup = marked('This is _Markdown_.');
-    return { __html: rawMarkup };
   }
 
 
@@ -59,6 +57,57 @@ class App extends React.Component {
 
   }}
 
+
+let placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
+marked.setOptions({
+  breaks: true });
 
 
 ReactDOM.render(
