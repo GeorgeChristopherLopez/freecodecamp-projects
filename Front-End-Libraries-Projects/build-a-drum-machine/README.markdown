39f740d2a3a037e9f1cfe6af09600382b66eecee
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-08-25 23:04:28 -0400

    Add files via upload

---
+++ Front-End-Libraries-Projects/build-a-drum-machine/README.markdown
@@ -0,0 +1,5 @@
+# Build a Drum Machine
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/gclopez/pen/XWdbeBg](https://codepen.io/gclopez/pen/XWdbeBg).
+
+
--- Front-End-Libraries-Projects/build-a-pomodoro-clock/dist/script.js
+++ Front-End-Libraries-Projects/build-a-pomodoro-clock/dist/script.js
@@ -5,10 +5,15 @@ function App() {
   const [onBreak, setOnBreak] = React.useState(false);
   const [running, setRunning] = React.useState(false);
 
+
+
   const format = time => {
     let min = Math.trunc(time / 60);
     let sec = time % 60;
-    return time;
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    return min + ":" + sec;
   };
 
   const reset = () => {
@@ -21,14 +26,18 @@ function App() {
   };
 
   const labelHanlder = command => {
-    console.log(command);
+
     if (command == "session-increment") {
-      if (sessionLength < 60)
-      setSessionLength(sessionLength + 1);
+      if (sessionLength < 60) {
+        setSessionLength(sessionLength + 1);
+        setTimeLeft((sessionLength + 1) * 60);
+      }
     }
     if (command == "session-decrement") {
-      if (sessionLength > 1)
-      setSessionLength(sessionLength - 1);
+      if (sessionLength > 1) {
+        setSessionLength(sessionLength - 1);
+        setTimeLeft((sessionLength - 1) * 60);
+      }
     }
     if (command == "break-increment") {
       if (breakLength < 60)
@@ -38,12 +47,30 @@ function App() {
       if (breakLength > 1)
       setBreakLength(breakLength - 1);
     }
-
+    if (onBreak) {
+      setTimeLeft(sessionLength);
+    }
   };
 
   const start_stop = () => {
     setRunning(!running);
   };
+
+  React.useEffect(() => {
+    let interval = null;
+    if (running && timeLeft !== 0) {
+      interval = setInterval(() => {
+        setTimeLeft(timeLeft => timeLeft - 1);
+      }, 1000);
+    } else if (!running && timeLeft !== 0) {
+      clearInterval(interval);
+    } else if (running && timeLeft == 0 && !onBreak) {
+      setOnBreak(true);
+      setTimeLeft(breakLength * 60);
+    }
+    return () => clearInterval(interval);
+  }, [running, timeLeft]);
+
 
 
   return (
@@ -53,26 +80,26 @@ function App() {
     React.createElement("h1", null, "Pomodoro Clock"),
     React.createElement("div", { id: "break-session-container" },
     React.createElement("div", { id: "break-label" },
-    React.createElement("a", { id: "break-increment", onClick: () => labelHanlder("break-increment") }, "+"),
-    React.createElement("a", { id: "break-length" }, breakLength),
-    React.createElement("a", { id: "break-decrement", onClick: () => labelHanlder("break-decrement") }, "-")),
+    React.createElement("a", { id: "break-increment", onClick: () => !running ? labelHanlder("break-increment") : null }, "+"),
+    React.createElement("p", { id: "break-length" }, breakLength),
+    React.createElement("a", { id: "break-decrement", onClick: () => !running ? labelHanlder("break-decrement") : null }, "-")),
 
     React.createElement("div", { id: "session-label" },
-    React.createElement("a", { id: "session-increment", onClick: () => labelHanlder("session-increment") }, "+"),
-    React.createElement("a", { id: "session-length" }, sessionLength),
-    React.createElement("a", { id: "session-decrement", onClick: () => labelHanlder("session-decrement") }, "-"))),
+    React.createElement("a", { id: "session-increment", onClick: () => !running ? labelHanlder("session-increment") : null }, "+"),
+    React.createElement("p", { id: "session-length" }, sessionLength),
+    React.createElement("a", { id: "session-decrement", onClick: () => !running ? labelHanlder("session-decrement") : null }, "-"))),
 
 
     React.createElement("div", { id: "timer-container" },
     React.createElement("h5", { id: "timer-label" }, onBreak ? "Break" : "Session"),
-    React.createElement("h1", { id: "time-left" }, timeLeft)),
+    React.createElement("h1", { id: "time-left" }, format(timeLeft))),
 
 
     React.createElement("div", { id: "controler" },
     React.createElement("a", { id: "start_stop", onClick: start_stop }, "start_stop"),
-    React.createElement("a", { id: "reset", onClick: reset }, "reset")),
+    React.createElement("a", { id: "reset", onClick: reset }, "reset"))));
 
-    running ? console.log(running) : React.createElement("p", null, "not running")));
+
 
 
 }
